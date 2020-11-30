50c934e3473e419449d48066214e8496937c979d
ian <ian@gregson.me>
2020-11-30 15:45:38 +0000

    day 14

--- 2018/day/14/solution.py
+++ 2018/day/14/solution.py
@@ -57,13 +57,42 @@ for i in range(N_RECIPES + AFTER_N):
 
 print('Part 1 answer:', "".join([str(d) for d in leaderboard[AFTER_N:AFTER_N + N_RECIPES]]))
 
+leaderboard = '37'
+pos_elf_1 = 0
+pos_elf_2 = 1
+
+def next_state(leaderboard, pos_elf_1, pos_elf_2):
+  leaderboard = add_recipes(leaderboard, pos_elf_1, pos_elf_2)
+  pos_elf_1, pos_elf_2 = move_elves(leaderboard, pos_elf_1, pos_elf_2)
+  return leaderboard, pos_elf_1, pos_elf_2
+
+def add_recipes(leaderboard, pos_elf_1, pos_elf_2):
+  score_elf_1 = int(leaderboard[pos_elf_1])
+  score_elf_2 = int(leaderboard[pos_elf_2])
+  score = score_elf_1 + score_elf_2
+  for d in str(score):
+    leaderboard += d
+  return leaderboard
+
+def move_elves(leaderboard, pos_elf_1, pos_elf_2):
+  n_moves_elf_1 = int(leaderboard[pos_elf_1]) + 1
+  n_moves_elf_2 = int(leaderboard[pos_elf_2]) + 1
+  pos_elf_1 = (pos_elf_1 + n_moves_elf_1) % len(leaderboard)
+  pos_elf_2 = (pos_elf_2 + n_moves_elf_2) % len(leaderboard)
+  return pos_elf_1, pos_elf_2
+
 target = input_txt
+# target = '51589'
+# target = '01245'
+# target = '92510'
 # target = '59414'
 
-leaderboard = '37'
+target_len = len(target)
+
 elf1 = 0
 elf2 = 1
-while target not in leaderboard[len(target):]:
+
+while target not in leaderboard[-target_len:]:
     leaderboard += str(int(leaderboard[elf1]) + int(leaderboard[elf2]))
     elf1 = (elf1 + int(leaderboard[elf1]) + 1) % len(leaderboard)
     elf2 = (elf2 + int(leaderboard[elf2]) + 1) % len(leaderboard)
