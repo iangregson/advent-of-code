d4b3d069ed2cfa443c45f68c5ef8c2da2f82c1f2
ian <ian@gregson.me>
2021-01-20 14:39:04 +0000

    day 24 pt2

--- 2020/day/24/solution.py
+++ 2020/day/24/solution.py
@@ -7,28 +7,28 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 file = open(dir_path + "/input.txt", "r")
 input_txt = [line.strip() for line in file.readlines()]
 
-input_txt = [
-  'sesenwnenenewseeswwswswwnenewsewsw',
-  'neeenesenwnwwswnenewnwwsewnenwseswesw',
-  'seswneswswsenwwnwse',
-  'nwnwneseeswswnenewneswwnewseswneseene',
-  'swweswneswnenwsewnwneneseenw',
-  'eesenwseswswnenwswnwnwsewwnwsene',
-  'sewnenenenesenwsewnenwwwse',
-  'wenwwweseeeweswwwnwwe',
-  'wsweesenenewnwwnwsenewsenwwsesesenwne',
-  'neeswseenwwswnwswswnw',
-  'nenwswwsewswnenenewsenwsenwnesesenew',
-  'enewnwewneswsewnwswenweswnenwsenwsw',
-  'sweneswneswneneenwnewenewwneswswnese',
-  'swwesenesewenwneswnwwneseswwne',
-  'enesenwswwswneneswsenwnewswseenwsese',
-  'wnwnesenesenenwwnenwsewesewsesesew',
-  'nenewswnwewswnenesenwnesewesw',
-  'eneswnwswnwsenenwnwnwwseeswneewsenese',
-  'neswnwewnwnwseenwseesewsenwsweewe',
-  'wseweeenwnesenwwwswnew',
-]
+# input_txt = [
+#   'sesenwnenenewseeswwswswwnenewsewsw',
+#   'neeenesenwnwwswnenewnwwsewnenwseswesw',
+#   'seswneswswsenwwnwse',
+#   'nwnwneseeswswnenewneswwnewseswneseene',
+#   'swweswneswnenwsewnwneneseenw',
+#   'eesenwseswswnenwswnwnwsewwnwsene',
+#   'sewnenenenesenwsewnenwwwse',
+#   'wenwwweseeeweswwwnwwe',
+#   'wsweesenenewnwwnwsenewsenwwsesesenwne',
+#   'neeswseenwwswnwswswnw',
+#   'nenwswwsewswnenenewsenwsenwnesesenew',
+#   'enewnwewneswsewnwswenweswnenwsenwsw',
+#   'sweneswneswneneenwnewenewwneswswnese',
+#   'swwesenesewenwneswnwwneseswwne',
+#   'enesenwswwswneneswsenwnewswseenwsese',
+#   'wnwnesenesenenwwnenwsewesewsesesew',
+#   'nenewswnwewswnenesenwnesewesw',
+#   'eneswnwswnwsenenwnwnwwseeswneewsenese',
+#   'neswnwewnwnwseenwseesewsenwsweewe',
+#   'wseweeenwnesenwwwswnew',
+# ]
 
 def parse(line):
   instruction = []
@@ -47,6 +47,8 @@ instructions = list(map(parse, input_txt))
 
 # axial coordinate system https://www.redblobgames.com/grids/hexagons/
 P = namedtuple('P', ['q', 'r'])
+WHITE = 0
+BLACK = 1
 directions = {
   'ne': P(1, -1),
   'e': P(1, 0),
@@ -61,58 +63,62 @@ def move(pos, direction):
   qq, rr = q + directions[direction].q, r + directions[direction].r
   return P(qq, rr)
 
-tiles = defaultdict(lambda: 'white')
+tiles = defaultdict(int)
 
 for instruction in instructions:
   pos = P(0,0)
   for direction in instruction:
     pos = move(pos, direction)
 
-  if tiles[pos] == 'white':
-    tiles[pos] = 'black'
+  if tiles[pos] == WHITE:
+    tiles[pos] = BLACK
   else:
-    tiles[pos] = 'white'
+    tiles[pos] = WHITE
 
 # print(tiles)
-print('Part 1:', list(tiles.values()).count('black'))
+print('Part 1:', sum(tiles.values()))
 
 def n_adjacent_black_tiles(pos, tiles, directions):
   adjacent_tiles = []
-  q, r = pos
-  for d in directions:
-    qq, rr = q + directions[d].q, r + directions[d].r
-    adjacent_tiles.append(tiles[P(qq, rr)])
-  # print(adjacent_tiles)
-  return adjacent_tiles.count('black')
+  for d in directions.values():
+    q, r = pos.q + d.q, pos.r + d.r
+    adjacent_tiles.append(tiles[P(q, r)])
+  return sum(adjacent_tiles)
 
 def flip_tiles(tiles, directions):
-  new_tiles = defaultdict(lambda: 'white')
+  new_tiles = tiles.copy()
 
-  for tile_pos in list(tiles):
-    tile = tiles[tile_pos]
-    n = n_adjacent_black_tiles(tile_pos, tiles, directions)
-    # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
-    if tile == 'black':
-      if n == 0 or n > 2:
-        new_tiles[tile_pos] = 'white'
-        print(tile, n, 'flipped to white')
-      else:
-        new_tiles[tile_pos] = 'black'
-        print(tile, n, 'stayed black')
-    # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
-    elif tile == 'white':
-      if n == 2:
-        new_tiles[tile_pos] = 'black'
-        print(tile, n, 'flipped black')
-      else:
-        new_tiles[tile_pos] = 'white'
-        print(tile, n, 'stayed white')
+  for q in range(-100,100):
+    for r in range(-100,100):
+      pos = P(q, r)
+      tile = tiles[pos]
+      n = n_adjacent_black_tiles(pos, tiles, directions)
+      # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+      if tile == BLACK:
+        if n == 0 or n > 2:
+          new_tiles[pos] = WHITE
+          # print(tile, n, 'flipped to white')
+        else:
+          new_tiles[pos] = BLACK
+          # print(tile, n, 'stayed black')
+      # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+      elif tile == WHITE:
+        if n == 2:
+          new_tiles[pos] = BLACK
+          # print(tile, n, 'flipped black')
+        else:
+          new_tiles[pos] = WHITE
+          # print(tile, n, 'stayed white')
   
   return new_tiles
 
-N_DAYS = 1
+N_DAYS = 100
 for i in range(1, N_DAYS + 1):
   tiles = flip_tiles(tiles, directions)
-  print(tiles)
-  print('Day {}:'.format(i), list(tiles.values()).count('black'))
+  # print(tiles)
+  # print(len(tiles))
+  if i % 10 == 0:
+    print('Day {}:'.format(i), sum(tiles.values()))
+
+print('Part 2:', sum(tiles.values()))
 
