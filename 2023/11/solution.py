b7fe71c2d4e5eceacd9b69dd54aa1ad9deddbcdf
ian <ian.gregson@bigtincan.com>
2023-12-11 11:46:05 +0000

    2023 day 11 (part2)

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -79,3 +79,66 @@ for a, b in galaxy_pairs:
 print(sum_d)
   
 
+# Part 2 ---------------------------------------
+# Needs to be 1 million times as big, so need to use a multiplier
+# don't actually redraw the space in memory
+
+space = text.splitlines()
+space = Grid(space)
+
+size_multiplier = 1e6
+# size_multiplier = 2
+
+galaxies = {}
+for y in range(space.height):
+  for x in range(space.width):
+    if space.row(y)[x] == '#':
+      n = len(galaxies)
+      galaxies[n] = (x,y)
+
+# print(galaxies)
+
+empty_rows: list[int] = []
+empty_cols: list[int] = []
+for x in range(space.width):
+  column = space.col(x)
+  if '#' not in column:
+    empty_cols.append(x)
+for y in range(space.height):
+  row = space.row(y)
+  if '#' not in row:
+    empty_rows.append(y)
+# print(empty_rows, empty_cols)
+
+
+def map_new_galaxy(pos: tuple[int, int], empty_cols: list[int], empty_rows: list[int]) -> tuple[int, int]:
+  x, y = pos
+  nexs = 0
+  for ex in empty_cols:
+    if x > ex:
+      nexs += 1
+  neys = 0
+  for ey in empty_rows:
+    if y > ey:
+      neys += 1
+
+  xx = x + (nexs * (size_multiplier - 1))
+  yy = y + (neys * (size_multiplier - 1))
+
+  return (xx, yy)
+
+for gal in galaxies:
+  pos = galaxies[gal]
+  new_pos = map_new_galaxy(pos, empty_cols, empty_rows)
+  # print(pos, new_pos)
+  galaxies[gal] = new_pos
+
+galaxy_pairs = list(itertools.combinations(galaxies.keys(), 2))
+sum_d = 0
+for a, b in galaxy_pairs:
+  ga, gb = galaxies[a], galaxies[b]
+  d = abs(ga[0] - gb[0]) + abs(ga[1] - gb[1])
+  sum_d += d
+
+print(sum_d)
+  
