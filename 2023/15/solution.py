912d1df736031fc14727fac154927e3ba213698a
ian <ian.gregson@bigtincan.com>
2023-12-15 11:30:35 +0000

    2023 day 15 (part 2)

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -6,10 +6,6 @@ file = pathlib.Path(__file__).parent.resolve() / 'input.txt'
 text = file.read_text()
 
 
-
-
-
-
 def hash(s: str) -> int:
   cv = 0
   for c in s:
@@ -20,7 +16,7 @@ def hash(s: str) -> int:
 
   return cv
 
-print(hash('HASH'))
+# print(hash('HASH'))
 
 seq = text
 parts = seq.split(',')
@@ -33,3 +29,61 @@ for part in parts:
 
 print(s)
 
+# Part 2 -----------------------------------------
+
+boxes = [[] for _ in range(256)]
+focal_map = {}
+
+seq = text
+parts = seq.split(',')
+
+for part in parts:
+  if part.endswith('-'):
+    p = part[:-1]
+    box_n = hash(p)
+    for i in range(len(boxes[box_n])):
+      box = boxes[box_n][i]
+      if box[0] == p:
+        boxes[box_n].pop(i)
+        break
+  else:
+    p, n = part.split('=')
+    box_n = hash(p)
+    focal_map[p] = int(n)
+    n = int(n)
+
+    found = False
+    for i in range(len(boxes[box_n])):
+      box = boxes[box_n][i]
+      if box[0] == p:
+        boxes[box_n].pop(i)
+        boxes[box_n].insert(i, (p,n))
+        found = True
+        break
+
+    if not found:
+      boxes[box_n].append((p,n))
+  
+  # print(part)
+  # for idx, box in enumerate(boxes):
+  #   if len(box) > 0:
+  #     print(idx, box)
+  # print()
+
+def focus_power(lens: tuple[str, int], box_n: int, box: list[tuple[str, int]]) -> int:
+  fp = 1 + box_n            # One plus the box number of the lens in question.
+  fp *= box.index(lens) + 1 # The slot number of the lens within the box: 1 for the first lens, 2 for the second lens, and so on.
+  fp *= lens[1]             # The focal length of the lens.
+  return fp
+
+
+ans = 0
+for box_n, box in enumerate(boxes):
+  if len(box) == 0:
+    continue
+
+  for lens in box:
+    fp = focus_power(lens, box_n, box)
+    ans += fp
+
+print(ans)
