3add9ad4dce90d650d6ea6dd1c3af55927f4b3d4
ian <ian.gregson@bigtincan.com>
2023-12-16 08:54:45 +0000

    2023 day 14 (part 2)

--- 2023/14/solution.py
+++ 2023/14/solution.py
@@ -1,4 +1,5 @@
-from functools import cache
+from collections import defaultdict
+from functools import reduce
 import pathlib
 file = pathlib.Path(__file__).parent.resolve() / 'input.txt'
 # file = pathlib.Path(__file__).parent.resolve() / 'example.txt'
@@ -18,24 +19,80 @@ for y in range(h):
     elif grid[y][x] == '#':
       cr.add((x,y))
 
+directions = {
+  (0,-1): 'N',
+  (-1,0): 'W',
+  (0,1):  'S',
+  (1,0):  'E',
+}
+
 rr = dict.fromkeys(sorted(rr))
-for rock in sorted(rr):
-  x,y = rock
-  yy = y - 1
-  while 0 <= yy < h:
-    next_pos = (x,yy)
-    if next_pos in rr:
-      break
-    if next_pos in cr:
-      break
-    
-    rr.pop(rock)
-    rr[next_pos] = None
-    rock = next_pos
-    yy -= 1
+def tilt(rr: dict[tuple[int, int], int], direction: tuple[int, int]) -> dict[tuple[int, int], int]:
+  global directions
+  # need to sort appropriately for direction
+  if directions[direction] == 'N':
+    srr = sorted(rr)
+  elif directions[direction] == 'S':
+    srr = sorted(rr, reverse=True)
+  elif directions[direction] == 'E':
+    srr = sorted(rr, key=lambda x: (x[0]), reverse=True)
+  elif directions[direction] == 'W':
+    srr = sorted(rr, key=lambda x: (x[0])) 
 
-ans = 0
-for x, y in sorted(rr):
-  ans += h - y
+  for rock in srr:
+    x,y = rock
+    dx, dy = direction
+    yy = y + dy
+    xx = x + dx 
+    while 0 <= yy < h and 0 <= xx < w:
+      next_pos = (xx,yy)
+      if next_pos in rr:
+        break
+      if next_pos in cr:
+        break
+      
+      rr.pop(rock)
+      rr[next_pos] = None
+      rock = next_pos
+      yy += dy
+      xx += dx
+  return rr
 
-print(ans)
+rr = tilt(rr, direction=(0,-1))
+def total_load(rr: dict[tuple[int, int], int]) -> int:
+  ans = 0
+  for x, y in sorted(rr):
+    ans += h - y
+  return ans
+
+print(total_load(rr))
+
+
+# Part 2 -------------------------------------
+
+def cycle(rr: dict[tuple[int, int], int]) -> dict[tuple[int, int], int]:
+  global directions
+  for direction in directions:
+    rr = tilt(rr, direction)
+
+  return rr
+
+loads = defaultdict(list[int])
+i = 0
+T = int(1e9)
+G = {}
+while i < T: 
+  i += 1
+  rr = cycle(rr)
+
+  hash = tuple(rr.keys())
+  if hash in G:
+    cycle_len = i - G[hash]
+    jump = (T - i) // cycle_len
+    i += jump * cycle_len
+  
+  G[tuple(rr.keys())] = i
+
+print(total_load(rr))
+# 100425 # low
+# 100782 # high
